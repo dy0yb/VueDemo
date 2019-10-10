aba3cba1dc0cb2760dda1041dcc91dd316d104af
dy0yb <15639927207@sina.cn>
2019-10-10 11:29:10 +0800

    Initial commit

---
+++ src-base/main.js
@@ -0,0 +1,13 @@
+// 入口js ， 创建Vue实例
+// untime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+// Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add/>
+      <List :comments="comments"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from './comporents/Add'
+import List from './comporents/List'
+export default {
+  data () {
+    return {
+      comments: [
+        {
+          name: 'BOB',
+          content: '不错'
+        },
+        {
+          name: 'Cat',
+          content: '不错'
+        },
+        {
+          name: 'BOB',
+          content: '不错'
+        }
+      ]
+    }
+  },
+  components: {List, Add}
+}
+</script>
+
+<style scoped>
+
+</style>
